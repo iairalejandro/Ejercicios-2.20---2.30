cf714d9d2f81551e42e94d509397d275f76c0b14
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2023-03-28 17:21:14 -0500

    Add files via upload

---
+++ Ejercicios-2.20---2.30/ej2-28.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2, num3 ,num4;
+
+    cout << "Ingrese 4 numeros: " << endl;
+    cin >> num1;
+    cin >> num2;
+    cin >> num3;
+    cin >> num4;
+
+    cout << num4 << "  " << num3 << "  " << num2 << "  " << num1 << "  " << endl;
+
+
+    return 0; 
+
+}
---
+++ Ejercicios-2.20---2.30/ej2-29.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    
+    cout << "Face length        Surface area         Volume \n";
+    cout << "of cube (cm)       of cube (cm^2)       of cube (cm^3) \n";
+    cout << "0                  0                    0 \n";
+    cout << "1                  6                    1 \n";
+    cout << "2                  24                   8 \n";
+    cout << "3                  54                   27 \n";
+    cout << "4                  96                   64 \n";
+
+    return 0;
+}
